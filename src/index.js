4a4bd47669f1670e185b322c3d937f66cb415bdf
Attila Gal <attila.gal@gmail.com>
2016-10-20 13:29:22 +0200

    setup basic skeleton project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-var HelloComponent = React.createClass({  
-    render: function() {
-        return <h1>Hello {this.props.name}</h1>;
-    }
-});
-
-ReactDOM.render(
-	<HelloComponent name="Evance"/>,
-	document.getElementById('root')
-);
+//TODO
